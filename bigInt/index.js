d3ffe03d285ac22a556fdf2b800e227a3f914ec5
Roger Zhang <1787009521@qq.com>
2022-07-04 14:19:18 +0800

    feat: 初步完成大数求和

---
+++ bigInt/index.js
@@ -0,0 +1,37 @@
+
+// 正确答案：111111111011111110
+function calculateNumber(strA, strB) {
+    const maxLength = Math.max(strA.length, strB.length)
+    let fullTen = false
+    let result = ''
+
+    for(let i=1; i<=maxLength; i++) {
+        let numberA = strA[maxLength - i] || '0' 
+        let numberB = strB[maxLength - i] || '0'
+        
+        let addNumber = parseInt(numberA) + parseInt(numberB)
+        
+        
+
+        if(fullTen) {
+            addNumber += 1
+        }
+
+        if(addNumber >= 10) {
+            addNumber -= 10
+            fullTen = true
+        } else {
+            fullTen = false
+        }
+        console.log(numberA, numberB, addNumber, 'addNumber')
+        result += addNumber
+    }
+    if(fullTen) {
+        result += 1
+    }
+
+    console.log(result)
+    return result
+}
+
+calculateNumber('12345678901234567', '98765432109876543')
